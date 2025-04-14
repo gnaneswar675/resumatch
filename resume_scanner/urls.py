0c9235c0e19655f1ba7e685bc04c91b22519840e
HemalathaBora <149257232+hemalathabora@users.noreply.github.com>
2025-04-14 13:19:14 +0530

    Update urls.py

--- resume_scanner/urls.py
+++ resume_scanner/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index'),
-    path ('joblist/', views.job_list, name='job_list'),
-    path ('apply/<int:job_id>/', views.apply_job, name='apply_job'),
-    path ('login/', views.login_view, name='login'),
-    path ('signup/', views.signup_view, name='signup'),
-path('logout/', views.logout_view, name='logout'),
-path('fraud-check/', views.fraud_check_view, name='fraud_check'),
-path('result/<int:pk>/', views.result_view, name='result_view'),
-path('scoreboard/<int:job_id>/', views.scoreboard_view, name='scoreboard'),
+    path('', views.index, name='index'),
+    path('joblist/', views.job_list, name='job_list'),  # Now returns static HTML
+    path('apply/<int:job_id>/', views.apply_job, name='apply_job'),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
+    path('logout/', views.logout_view, name='logout'),
+    path('fraud-check/', views.fraud_check_view, name='fraud_check'),
+    path('result/<int:pk>/', views.result_view, name='result_view'),
+    path('scoreboard/<int:job_id>/', views.scoreboard_view, name='scoreboard'),
 ]
